5be4cfb54d13570e354ebc5e68bc48f16cce5fa3
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-02-11 22:54:32 +0200

    setup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-// Import thr React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// Create a react component
 const App = () => {
-    const buttonText = {text: 'Click me!'}
-    const labelText = 'Enter name:'
-
     return (
-        <div>
-            <label className="label" htmlFor="name">
-             {labelText}
-            </label>
-            <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>
-                {buttonText.text}
-            </button>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00 pm</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
         </div>
     )
-}
+};
 
-// Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'))
