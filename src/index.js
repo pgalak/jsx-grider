13ead7a698e8b93a1d667032c8613f5a07422201
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-02-14 12:55:35 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00 pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="ui container comments">
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					content="Nice blog post"
+					avatar={faker.image.image()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:00AM"
+					content="I like the subject"
+					avatar={faker.image.image()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 5.00PM"
+					content="I like the writing"
+					avatar={faker.image.image()}
+				/>
+			</ApprovalCard>
+		</div>
+	)
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'))
