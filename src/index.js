4c2f24b9a9b945d64f298edc6c28ab2d7e0ba00d
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-02-14 21:11:37 +0200

    sections: 4, 5

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
-import CommentDetail from './CommentDetail'
-import ApprovalCard from './ApprovalCard'
 
-const App = () => {
-	return (
-		<div className="ui container comments">
-			<ApprovalCard>
-				<CommentDetail
-					author="Sam"
-					timeAgo="Today at 4:45PM"
-					content="Nice blog post"
-					avatar={faker.image.image()}
-				/>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail
-					author="Alex"
-					timeAgo="Today at 2:00AM"
-					content="I like the subject"
-					avatar={faker.image.image()}
-				/>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail
-					author="Jane"
-					timeAgo="Yesterday at 5.00PM"
-					content="I like the writing"
-					avatar={faker.image.image()}
-				/>
-			</ApprovalCard>
-		</div>
-	)
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+    // ! this is the only time we do direct assignment to
+    // ! this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // ! We called setState!!!
+        this.setState({ lat: position.coords.latitude })
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // ! React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    return <div>Loading!</div>
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
