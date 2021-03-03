66cb607d901cb7c4848307c0d46e4731e36d47e8
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-03-03 21:37:57 +0200

    added convert and translate

---
+++ src/components/Translate.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Dropdown from './Dropdown'
+import Convert from './Convert'
+
+const options = [
+  { label: 'Afrikaans', value: 'af' },
+  { label: 'Arabic', value: 'ar' },
+  { label: 'Hindi', value: 'hi' },
+  { label: 'Dutch', value: 'nl' },
+]
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0])
+  const [text, setText] = useState('')
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        </div>
+      </div>
+      <Dropdown
+        label={'Select a language'}
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options} />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert language={language} text={text}/>
+    </div>
+  )
+}
+
+export default Translate
