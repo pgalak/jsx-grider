d4e4df3f23ebc50d6fdf8393cb3d216387852489
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-03-06 22:47:11 +0200

    minor fix

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>Vide Search</label>
+          <label>Search for a video</label>
           <input
             type="text"
             value={term}
