ff7df941a53a35fd5d9e9d3af772ec8bf93b359c
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-03-10 19:14:36 +0200

    SongDetails

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import SongList from './SongList'
+import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
@@ -8,9 +9,12 @@ const App = () => {
         <div className="column eight wide">
           <SongList />
         </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,23 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
 
 const songsReducer = () => {
   return [
-    { title: 'No Scrubs', duration: '4:05'},
-    { title: 'Macarena', duration: '2:30'},
-    { title: 'All Start', duration: '3:14'},
-    { title: 'I want it that way', duration: '1:45'},
-  ]
-}
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' }
+  ];
+};
 
 const selectedSongReducer = (selectedSong = null, action) => {
-  if(action.type === 'SONG_SELECTED') {
-    return action.payload
-  } 
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
 
-  return selectedSong
-}
+  return selectedSong;
+};
 
 export default combineReducers({
   songs: songsReducer,
   selectedSong: selectedSongReducer
-})
+});
