1c01e0ae8c96f4ef20d5425722c5df5b23bd037f
Panagiotis Tagkalakis <panagiotis.tagkalakis@eurodyn.com>
2021-03-10 18:58:40 +0200

    added SongList

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import SongList from './SongList'
 
 const App = () => {
-
   return (
-    <div className="ui container">
-      
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   )
 }
